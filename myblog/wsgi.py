4d4539bb8c36b80c7d733aace8e49d7e51311c0e
Manokamna kumar singh <manokamnasingh2001@gmail.com>
2021-07-09 18:00:09 +0530

    third commit

--- myblog/wsgi.py
+++ myblog/wsgi.py
@@ -13,4 +13,4 @@ from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myblog.settings')
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
