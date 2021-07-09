6f01dc984c3d012501d747cbe82b7b22baf1a82d
Manokamna kumar singh <manokamnasingh2001@gmail.com>
2021-07-09 22:29:26 +0530

    eleven commit

--- myblog/settings.py
+++ myblog/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles')
+    os.path.join(BASE_DIR, 'static')
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
