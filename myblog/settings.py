b9ff0945bfa9d31d0dad29d4b087d471a2ecd4c0
Manokamna kumar singh <manokamnasingh2001@gmail.com>
2021-07-09 19:55:18 +0530

    fourth commit

--- myblog/settings.py
+++ myblog/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["codeinsight21.herokuapp.com","localhost" ]
+ALLOWED_HOSTS = ["codeinsight21.herokuapp.com","127.0.0.1" ]
 
 
 # Application definition
