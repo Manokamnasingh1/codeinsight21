2ed2f0aa209427757d7aab413454bd01e3ccbeb9
Manokamna kumar singh <manokamnasingh2001@gmail.com>
2021-07-10 11:31:50 +0530

    third commit

--- myblog/urls.py
+++ myblog/urls.py
@@ -15,10 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 
+from django.views.static import serve
+from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
-    path('blog/', include('blog.urls'))
-
+    path('blog/', include('blog.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
+
+
