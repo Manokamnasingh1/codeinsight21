d7b4d68a850979a14088cad64900fc1d6e8dc9d9
Manokamnasingh1 <85088233+Manokamnasingh1@users.noreply.github.com>
2021-07-28 18:00:21 +0530

    Update settings.py

--- myblog/settings.py
+++ myblog/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY ='@=$-e#ww!!)0)-&jyuj&^o@uc0b)016s2e^+$vcqew*$6=&ju1'
 DEBUG = False
 
 
-ALLOWED_HOSTS = ['codeinsight1.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
