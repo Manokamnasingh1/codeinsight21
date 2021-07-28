b810b975edcd48e2ef589018dd88953d18cd5dbe
Manokamnasingh1 <85088233+Manokamnasingh1@users.noreply.github.com>
2021-07-28 16:18:33 +0530

    Create settings.py

--- myblog/settings.py
+++ myblog/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY ='@=$-e#ww!!)0)-&jyuj&^o@uc0b)016s2e^+$vcqew*$6=&ju1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = []
